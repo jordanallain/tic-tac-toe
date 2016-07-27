ec3e40403a5fd0c0388192b2e88e91cfc0299591
jordanallain <jallain@gmail.com>
2016-07-26 22:56:26 -0400

    Nearly finish HTML, CSS, and Nav Bar
    Ready to merge with master branch. HTML, CSS, and Nav Bar with
    functioning sign in, sign out, change password, and sign up
    nearly done. May need fine-tuning at the end of the project.

---
+++ assets/scripts/game-ui.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const attachGameLogic = () => {
+  //add win checkers
+};
+
+module.exports = attachGameLogic;
+
+// const app = require('../app');
+
+
+
+// const signOut = () => {
+//   return $.ajax({
+//     url: app.api + '/sign-out/' + app.user.id,
+//     method: "DELETE",
+//     headers: {
+//       Authorization: 'Token token=' + app.user.token,
+//     },
+//   });
+// };
